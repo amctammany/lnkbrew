6241fd3dc67afbc3273813e6be8e174dfca4edc3
Alex McTammany <apmctammany@gmail.com>
2024-03-10 07:18:26 -0400

    added variants for navlink

--- components/Nav/NavLink.tsx
+++ components/Nav/NavLink.tsx
@@ -4,11 +4,11 @@ import clsx from "clsx";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-const navLinkStyles = cva(["block text-center font-bold hover:text-red-500"], {
+const navLinkStyles = cva(["block text-center font-bold "], {
   variants: {
     variant: {
-      default: ["text-blue-400"],
-      subnav: ["text-green-300"],
+      default: ["text-white hover:text-red-500"],
+      subnav: ["text-blue-500 hover:text-blue-700 hover:underline"],
     },
     active: {
       active: ["text-yellow-500"],
@@ -20,6 +20,7 @@ const navLinkStyles = cva(["block text-center font-bold hover:text-red-500"], {
     },
   },
   defaultVariants: {
+    variant: "default",
     active: "active",
     size: "default",
   },
@@ -29,10 +30,16 @@ export type NavLinkProps = VariantProps<typeof navLinkStyles> & {
   children: React.ReactNode;
   className?: string;
 };
-export const NavLink = ({ children, href, className, size }: NavLinkProps) => {
+export const NavLink = ({
+  variant,
+  children,
+  href,
+  className,
+  size,
+}: NavLinkProps) => {
   const pathname = usePathname();
   const active = href === pathname.slice(0, href.length) ? "active" : "default";
-  const c = clsx(navLinkStyles({ active, size }), className);
+  const c = clsx(navLinkStyles({ active, size, variant }), className);
   return (
     <Link href={href} className={c}>
       {children}
