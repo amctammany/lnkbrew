306770a8deee51dfec37f3d7b1b58928d20b452f
Alex McTammany <apmctammany@gmail.com>
2024-05-30 00:37:25 -0400

    cleanup?

--- components/Table/DataColumn.tsx
+++
@@ -1,7 +0,0 @@
-import { DataColumnProps } from "./types";
-
-export function DataColumn<
-  T extends Record<string, any> = Record<string, any>,
->({ label }: DataColumnProps<T>) {
-  return <th>{label}</th>;
-}
