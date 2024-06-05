e63b4084d661852d7a2428cf3ae2fb6cb2bd047d
Alex McTammany <apmctammany@gmail.com>
2024-06-05 00:58:51 -0400

    fixed mash profile actoins

--- app/profiles/mash/actions.ts
+++ app/profiles/mash/actions.ts
@@ -8,35 +8,45 @@ import { zfd } from "zod-form-data";
 const mashSchema = zfd.formData({
   id: zfd.numeric(z.number().optional()),
   name: zfd.text(),
-  userId: zfd.text(z.string().optional()),
-  forkedFrom: zfd.numeric(z.number().optional()),
-
   description: zfd.text(z.string().optional()),
-  calcium: zfd.numeric(z.number().min(0).default(0)),
-  magnesium: zfd.numeric(z.number().min(0).default(0)),
-  sodium: zfd.numeric(z.number().min(0).default(0)),
-  chloride: zfd.numeric(z.number().min(0).default(0)),
-  sulfate: zfd.numeric(z.number().min(0).default(0)),
-  bicarbonate: zfd.numeric(z.number().min(0).default(0)),
+  steps: z.array(
+    z.object({
+      name: zfd.text(z.string().optional()),
+      temperature: zfd.numeric(z.number().min(0).max(212)),
+      time: zfd.numeric(z.number().min(0)),
+      rampTime: zfd.numeric(z.number().min(0).default(0)),
+    })
+  ),
 });
 export const createMashProfile = async (formData: FormData) => {
-  const data = mashSchema.parse(formData);
+  const { steps, ...data } = mashSchema.parse(formData);
   const res = await prisma.mashProfile.create({
     data: {
       ...data,
       slug: slugify(data.name, { lower: true }),
+      steps: {
+        createMany: { data: steps },
+      },
     },
+    include: { steps: true },
   });
   redirect(`/profiles/mash/${res.slug}`);
 };
 export const updateMashProfile = async (formData: FormData) => {
-  const data = mashSchema.parse(formData);
+  const { steps, ...data } = mashSchema.parse(formData);
   const res = await prisma.mashProfile.update({
     where: { id: data.id },
     data: {
       ...data,
       slug: slugify(data.name, { lower: true }),
+      steps: {
+        deleteMany: {
+          mashProfileId: data.id,
+        },
+        createMany: { data: steps },
+      },
     },
+    include: { steps: true },
   });
   redirect(`/profiles/mash/${res.slug}`);
 };
