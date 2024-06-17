754f09314e1d6684830464a014c572a7d4d2ada2
Alex McTammany <apmctammany@gmail.com>
2024-06-17 01:16:55 -0400

    checkin

--- app/styles/_components/StylesList/StylesList.tsx
+++ app/styles/_components/StylesList/StylesList.tsx
@@ -1,7 +1,6 @@
 import { List } from "@/components/List/List";
 import { Style } from "@prisma/client";
 import { StyleListItem } from "./StyleListItem";
-import { Section } from "@/components/Section";
 
 export type StylesListProps = {
   styles: Style[];
@@ -9,12 +8,10 @@ export type StylesListProps = {
 
 export const StylesList = ({ styles }: StylesListProps) => {
   return (
-    <Section title="Styles">
-      <List className="p-4">
-        {styles.map((style) => (
-          <StyleListItem key={style.id} style={style} />
-        ))}
-      </List>
-    </Section>
+    <List className="p-4">
+      {styles.map((style) => (
+        <StyleListItem key={style.id} style={style} />
+      ))}
+    </List>
   );
 };
