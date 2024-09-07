95496012b932a7b9c3d11475c23c94d86a50336d
Alex McTammany <apmctammany@gmail.com>
2024-09-06 21:59:03 -0400

    added ordering f rlabel

--- components/Label/Label.tsx
+++ components/Label/Label.tsx
@@ -7,17 +7,23 @@ export type LabelProps = VariantProps<typeof labelStyles> & {
 const labelStyles = cva(["inline-flex gap-1"], {
   variants: {
     variant: {
-      default: "",
+      default: "text-inherit",
+      subnav: "text-blue-300",
       warning: "text-warning-500",
+    },
+    order: {
+      default: "flex-row",
+      reverse: "flex-row-reverse",
     },
   },
   defaultVariants: {
     variant: "default",
+    order: "default",
   },
 });
-export const Label = ({ text, variant, children }: LabelProps) => {
+export const Label = ({ text, variant, order, children }: LabelProps) => {
   return (
-    <div className={labelStyles({ variant })}>
+    <div className={labelStyles({ variant, order })}>
       {children}
       <span>{text}</span>
     </div>
