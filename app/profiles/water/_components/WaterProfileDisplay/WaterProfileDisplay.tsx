5a4989a0310f8f879310d6c4eaa88fdb9d4bb83a
Alex McTammany <apmctammany@gmail.com>
2024-09-13 23:46:22 -0400

    fixed water profile displa;y to use untis

--- app/profiles/water/_components/WaterProfileDisplay/WaterProfileDisplay.tsx
+++ app/profiles/water/_components/WaterProfileDisplay/WaterProfileDisplay.tsx
@@ -67,24 +67,12 @@ export const WaterProfileDisplay = ({
       <div className="p-4">
         <Prop label="Name">{profile?.name}</Prop>
         <Prop label="Description">{profile?.description}</Prop>
-        <Prop label="Calcium" unit="ppm">
-          {profile?.calcium}
-        </Prop>
-        <Prop label="Magnesium" unit="ppm">
-          {profile?.magnesium}
-        </Prop>
-        <Prop label="Sodium" unit="ppm">
-          {profile?.sodium}
-        </Prop>
-        <Prop label="Sulfate" unit="ppm">
-          {profile?.sulfate}
-        </Prop>
-        <Prop label="Chloride" unit="ppm">
-          {profile?.chloride}
-        </Prop>
-        <Prop label="Bicarbonate" unit="ppm">
-          {profile?.bicarbonate}
-        </Prop>
+        <Prop label="Calcium" unit="ppm" value={profile?.calcium} />
+        <Prop label="Magnesium" unit="ppm" value={profile?.magnesium} />
+        <Prop label="Sodium" unit="ppm" value={profile?.sodium} />
+        <Prop label="Sulfate" unit="ppm" value={profile?.sulfate} />
+        <Prop label="Chloride" unit="ppm" value={profile?.chloride} />
+        <Prop label="Bicarbonate" unit="ppm" value={profile?.bicarbonate} />
       </div>
     </Section>
   );
