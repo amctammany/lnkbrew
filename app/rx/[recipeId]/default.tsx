c7e815aa940f79fbc0ee9f5056f22afdb2ae3bfc
Alex McTammany <apmctammany@gmail.com>
2024-09-15 06:26:29 -0400

    added def for children slot

--- app/rx/[recipeId]/default.tsx
+++ app/rx/[recipeId]/default.tsx
@@ -1,3 +1,8 @@
-export default function Default() {
-  return null;
+import RecipeEditor from "../_components/RecipeEditor/RecipeEditor";
+
+export default function Default({ params }: { params: { path?: any } }) {
+  //return null;
+  console.log(params);
+  const recipe = {} as any;
+  return <RecipeEditor recipe={recipe} path={params.path} />;
 }
