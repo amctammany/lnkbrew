4b075d6124501252382c3c7dd25d82c899f5e63a
Alex McTammany <apmctammany@gmail.com>
2024-10-14 00:22:19 -0400

    works?

--- app/rx/[recipeId]/default.tsx
+++ app/rx/[recipeId]/default.tsx
@@ -2,7 +2,7 @@ import { ExtendedRecipe } from "@/types/Recipe";
 import RecipeEditor from "../_components/RecipeEditor/RecipeEditor";
 
 export default function Default() {
-  //return null;
-  const recipe = {} as ExtendedRecipe;
-  return <RecipeEditor recipe={recipe} />;
+  return null;
+  //const recipe = {} as ExtendedRecipe;
+  //return <RecipeEditor recipe={recipe} />;
 }
